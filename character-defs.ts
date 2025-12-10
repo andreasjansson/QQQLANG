1e71171b257993c36a74d91e300666370474f453
Andreas Jansson <ajansson@cloudflare.com>
2025-12-10 15:10:15 +0100

    Fix horizontal flip in fn5 vertex shader

--- character-defs.ts
+++ character-defs.ts
@@ -1679,7 +1679,7 @@ function fn5(ctx: FnContext, n: number): Image {
     attribute vec2 position;
     varying vec2 vUV;
     void main() {
-      vUV = position * 0.5 + 0.5;
+      vUV = vec2(position.x * 0.5 + 0.5, 1.0 - (position.y * 0.5 + 0.5));
       gl_Position = vec4(position, 0.0, 1.0);
     }
   `;
