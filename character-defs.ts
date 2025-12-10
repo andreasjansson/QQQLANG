4d861f82bd46c110c58f52760bf656046b6571ee
Andreas Jansson <ajansson@cloudflare.com>
2025-12-10 23:39:07 +0100

    Use old_image for L tube texture

--- character-defs.ts
+++ character-defs.ts
@@ -978,7 +978,7 @@ function fnL(ctx: FnContext, j: number, rot: number): Image {
   gl.activeTexture(gl.TEXTURE1);
   const tubeTexture = gl.createTexture();
   gl.bindTexture(gl.TEXTURE_2D, tubeTexture);
-  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, prev.width, prev.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, prev.data);
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, old.width, old.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, old.data);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
