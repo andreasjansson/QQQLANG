7b1d8b541e6ecfff0caa01bdf89b14fdd3be74ad
Andreas Jansson <ajansson@cloudflare.com>
2025-12-12 22:16:07 +0100

    Fix blur vertical flip by removing double flip

--- character-defs.ts
+++ character-defs.ts
@@ -4044,7 +4044,7 @@ function fnBlur(ctx: FnContext, n: number): Image {
     attribute vec2 position;
     varying vec2 vUV;
     void main() {
-      vUV = vec2(position.x * 0.5 + 0.5, 1.0 - (position.y * 0.5 + 0.5));
+      vUV = vec2(position.x * 0.5 + 0.5, position.y * 0.5 + 0.5);
       gl_Position = vec4(position, 0.0, 1.0);
     }
   `;
