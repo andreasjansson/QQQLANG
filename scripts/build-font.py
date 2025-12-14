b56c21e60a6181644b94841533d67c808ed68240
Andreas Jansson <ajansson@cloudflare.com>
2025-12-14 16:21:00 +0100

    fix: pass upload_chars to build_gsub_feature

--- scripts/build-font.py
+++ scripts/build-font.py
@@ -409,7 +409,7 @@ def build_font():
     build_gsub_feature(font, char_defs, qqqlang_chars, arity_map,
                        glyph_name_map, bold_first_glyph_map, bold_glyph_map, 
                        bold_spaced_glyph_map, regular_spaced_glyph_map,
-                       upload_variants)
+                       upload_chars)
     
     print("Updating font names...")
     name_table = font['name']
