7c5978b228faa19d9c16c17413243b3c9946c7e1
Andreas Jansson <ajansson@cloudflare.com>
2025-12-15 22:29:23 +0100

    Increase max skew angle to 85 degrees

--- functions/skew.ts
+++ functions/skew.ts
@@ -22,7 +22,7 @@ function skew(ctx: FnContext, amount: number): Image {
   const out = createSolidImage(ctx.width, ctx.height, "#000000");
 
   const normalizedAmount = ((amount - 1) / 67) * 2 - 1;
-  const maxSkewAngle = 70;
+  const maxSkewAngle = 85;
   const skewAngle = -normalizedAmount * maxSkewAngle;
   const skewAmount = (Math.tan((skewAngle * Math.PI) / 180) * ctx.height) / 2;
 
