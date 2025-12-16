3e4cff481ec3949b91e448dd8589da9bc5c8249d
Andreas Jansson <ajansson@cloudflare.com>
2025-12-16 18:28:30 +0100

    Call Python script to generate swatches and use them in README

--- scripts/readme/generate-readme.ts
+++ scripts/readme/generate-readme.ts
@@ -2,6 +2,7 @@ import { chromium, type Page } from "playwright";
 import * as fs from "fs";
 import * as path from "path";
 import { fileURLToPath } from "url";
+import { execSync } from "child_process";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
