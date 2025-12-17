82ff179dc2b1428e3a80b738d172c7d77ab0bbec
Andreas Jansson <ajansson@cloudflare.com>
2025-12-17 14:19:58 +0100

    Remove remaining --- lines from README generation

--- scripts/readme/generate-readme.ts
+++ scripts/readme/generate-readme.ts
@@ -305,8 +305,6 @@ Some functions take an image index as an argument, and uses that old image in so
 </tr>
 </table>
 
----
-
 # About
 
 QQQ is short for QQQEJOTTONO, a word that my three-year old son wrote on a label maker. He then went on to write fifty or so other words, until the label roll ran out. I thought it'd be nice if these labels could be treated like code.
